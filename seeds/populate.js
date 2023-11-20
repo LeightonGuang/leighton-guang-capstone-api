ada1bdc314b1971eff895371f39852b828b074c5
Leighton Guang <leighton.guang@gmail.com>
2023-11-20 16:31:29 +0000

    fix: populate works now

--- seeds/populate.js
+++ seeds/populate.js
@@ -3,16 +3,13 @@
  * @returns { Promise<void> }
  */
 
-const shopData = require("../seed-data/products");
+const shopData = require("../seed-data/shops");
+const productData = require("../seed-data/products");
 
 exports.seed = async function (knex) {
   await knex("shop").del();
   await knex("shop").insert(shopData);
-};
 
-const productData = require("../seed-data/products");
-
-exports.seed = async function (knex) {
   await knex("product").del();
   await knex("product").insert(productData);
 };
