d0ac119e401b3b2641c70aade0aa8064b526aab1
Leighton Guang <leighton.guang@gmail.com>
2023-11-20 15:45:24 +0000

    fix: edit seed data

--- seed-data/products.js
+++ seed-data/products.js
@@ -1,50 +1,110 @@
 module.exports = [
   {
-    id: 1,
     name: "i5",
+    img_url:
+      "https://www.price.com.hk/space/product/563000/563906_lztgc4_4.jpg",
     brand: "Intel",
     model: "13600k",
-    category: "cpu",
+    category: "CPU",
     description: "13th gen Intel cpu",
   },
   {
-    id: 2,
     name: "i7",
+    img_url:
+      "https://www.price.com.hk/space/product/563000/563902_i6vyiq_4.jpg",
     brand: "Intel",
     model: "13700k",
-    category: "cpu",
+    category: "CPU",
     description: "13th gen Intel cpu",
   },
   {
-    id: 3,
     name: "i9",
+    img_url:
+      "https://www.price.com.hk/space/product/563000/563899_kz5f5x_4.jpg",
     brand: "Intel",
     model: "13900k",
-    category: "cpu",
+    category: "CPU",
     description: "13th gen Intel cpu",
   },
   {
-    id: 4,
     name: "i5",
+    img_url:
+      "https://www.price.com.hk/space/product/603000/603195_4zyg2i_4.jpg",
     brand: "Intel",
     model: "14600k",
-    category: "cpu",
+    category: "CPU",
     description: "14th gen Intel cpu",
   },
   {
-    id: 5,
     name: "i7",
+    img_url:
+      "https://www.price.com.hk/space/product/603000/603197_buvjzj_4.png",
     brand: "Intel",
     model: "14700k",
-    category: "cpu",
+    category: "CPU",
     description: "14th gen Intel cpu",
   },
   {
-    id: 6,
     name: "i9",
+    img_url:
+      "https://www.price.com.hk/space/product/603000/603200_qeuyf7_4.jpg",
     brand: "Intel",
     model: "14900k",
-    category: "cpu",
+    category: "CPU",
     description: "14th gen Intel cpu",
+  },
+  {
+    name: "ASUS Dual GeForce RTX 3060 V2 OC Edition",
+    img_url:
+      "https://www.price.com.hk/space/product/504000/504235_9qr2ff_4.png",
+    brand: "ASUS",
+    model: "RTX 3060",
+    category: "Graphics Card",
+    description: "graphics card",
+  },
+  {
+    name: "MSI GeForce RTX 3060 GAMING X 12G",
+    img_url:
+      "https://www.price.com.hk/space/product/489000/489076_awctpx_4.png",
+    brand: "MSI",
+    model: "RTX 3060",
+    category: "Graphics Card",
+    description: "graphics card",
+  },
+  {
+    name: "Gigabyte GeForce RTX 3060 GAMING OC 12G",
+    img_url:
+      "https://www.price.com.hk/space/product/490000/490800_owwrod_4.png",
+    brand: "Gigabyte",
+    model: "RTX 3060",
+    category: "Graphics Card",
+    description: "graphics card",
+  },
+  {
+    name: "AMD Ryzen 5 5600X",
+    img_url:
+      "https://www.price.com.hk/space/product/475000/475159_c64d6e_4.jpg",
+    brand: "AMD",
+    model: "5600X",
+    category: "CPU",
+    description: "AM5 socket cpu",
+  },
+  {
+    name: "AMD Ryzen 7 7800X3D",
+    img_url:
+      "https://www.price.com.hk/space/product/581000/581469_a4a9h9_4.png",
+    brand: "AMD",
+    model: "7800X3D",
+    category: "CPU",
+    description: "AM5 socket cpu",
+  },
+  {
+    name: "AMD Ryzen 9 7950X",
+    img_url:
+      "https://www.price.com.hk/space/product/560000/560905_x80trv_4.png",
+    brand: "AMD",
+    model: "7950X",
+    category: "CPU",
+    description: "AM5 socket cpu",
   },
 ];
--- seed-data/shops.js
+++ seed-data/shops.js
@@ -1,6 +1,5 @@
 module.exports = [
   {
-    id: 1,
     name: "Super Store",
     email: "superStore@gmail.com",
     password: "pmwvetpou",
