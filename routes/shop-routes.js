7eda7c646ed9b54a0cdd46f5d6308086ac5b89af
Leighton Guang <leighton.guang@gmail.com>
2023-11-28 23:54:23 +0000

    fix: routing order causes authentication issue

--- routes/shop-routes.js
+++ routes/shop-routes.js
@@ -3,9 +3,10 @@ const shopController = require("../controllers/shop-controller");
 const authenticate = require("../middleware/authenticate");
 
 router.route("/").get(shopController.getAllShop);
-router.route("/:id").get(shopController.getListingofAShop);
 router.route("/listing").get(shopController.getAllListing);
 router.route("/listing").post(shopController.addListing);
 router.route("/profile").get(authenticate, shopController.profile);
+router.route("/:id/listing").get(authenticate, shopController.manageListing);
+router.route("/:id").get(shopController.getListingofAShop);
 
 module.exports = router;
