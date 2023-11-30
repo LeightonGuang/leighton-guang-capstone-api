030228fdf9e476f3e4711bbd44a6e1380f6ac9db
Leighton Guang <leighton.guang@gmail.com>
2023-11-30 14:44:30 +0000

    feat: new routes

--- routes/product-routes.js
+++ routes/product-routes.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const productController = require("../controllers/product-controller");
+const authenticate = require("../middleware/authenticate");
 
 router.route("/").get(productController.getAllProduct);
+router.route("/").post(authenticate, productController.createNewProduct);
 router.route("/category").get(productController.getAllProductCategory);
 router.route("/category/:id").get(productController.getAllProductInCategory);
 router.route("/:id").get(productController.getProductDetails);
--- routes/shop-routes.js
+++ routes/shop-routes.js
@@ -4,9 +4,12 @@ const authenticate = require("../middleware/authenticate");
 
 router.route("/").get(shopController.getAllShop);
 router.route("/listing").get(shopController.getAllListing);
-router.route("/listing").post(shopController.addListing);
+router.route("/listing").post(authenticate, shopController.addListing);
 router.route("/profile").get(authenticate, shopController.profile);
 router.route("/:id/listing").get(authenticate, shopController.manageListing);
+router
+  .route("/delete/:listing_id")
+  .delete(authenticate, shopController.shopDeleteListing);
 router.route("/:id").get(shopController.getListingofAShop);
 
 module.exports = router;
